1c322528bf9078fc6688eae08c89ca058a3b51ab
preciouswanjiku <precious2shiko@gmail.com>
2026-02-05 05:03:40 +0300

    updated file

---
+++ Assignment1.py
@@ -0,0 +1,20 @@
+# Write a python that is able to culculate the BMI of a person whose weight is 78kgs and height is 1.75 meters. BMI = (Weight)/ (Height Squared)
+
+number1 = 78
+number2 = 1.75
+BMI = number1 / number2 * number2
+print("The BMI is:", BMI)
+
+# Find the area and perimeter of a rectangle whose length is 48cm and width is 25cm
+number3 = 48
+number4 = 25
+perimeter = 4 * number3 + number4
+print("The perimeter is:", perimeter)
+
+Area = number3 * number4
+print("The area is:", Area)
+
+# Research on python list, tuple and dictionary Data types
+# Python list is a way to store multiple items in one variable, for example, Ordered list in which the items are kept in their position , changable list, in which you can add remove or edit items, and allow duplicate list, in which some items can appear more than once.
+
+# A dictionary data type, is used to store data in key-value pairs
